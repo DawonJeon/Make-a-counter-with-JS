63a8af57d3711c14a63604c4fcb667c027e8827c
DawonJeon <dawon3024@naver.com>
2022-01-14 19:54:33 +0900

    make a counter with JS

--- src/counter.js
+++ src/counter.js
@@ -3,14 +3,16 @@ let counterDisplay = document.querySelector('.number');
 let counterMinus = document.querySelector('.subOne');
 let counterPlus = document.querySelector('.addOne');
 
-let count = 0;
 
 counterPlus.addEventListener("click",() => {
-    count += 1;
-    counterDisplay.innerHTML = count;
+    // count += 1;
+    // parseInt(counterDisplay);
+    let counterDisplayToNumber = Number(counterDisplay.innerText)
+    counterDisplayToNumber += 1;
+    counterDisplay.innerText = counterDisplayToNumber;
 })
 
 counterMinus.addEventListener("click",() =>{
-    count -= 1;
-    counterDisplay.innerHTML = count;
+    // count -= 1;
+    counterDisplay.innerText -= 1;
 })
